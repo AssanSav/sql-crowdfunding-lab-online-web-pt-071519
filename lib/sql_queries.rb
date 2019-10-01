1548caa96c15d5c522dbd7d4f20da91388e62077
assane savadogo <assane.savadogo81@gmail.com>
2019-10-01 16:09:08 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,8 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, (SUM(pledges.funding_goal) - projects.funding_goal) AS result FROM projects 
+  "SELECT projects.title, (SUM(pledges.funding_goal) - projects.funding_goal) AS result 
+  FROM projects 
   JOIN pledges ON pledges.id = pledges.project_id
   GROUP BY result <= 0"
 end
